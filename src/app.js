a4f512c1bfaad882752e0ecd3d8502a4219dd89a
em-wb <131539794+em-wb@users.noreply.github.com>
2023-10-25 17:10:31 +0200

    Edit: Tidy code

--- src/app.js
+++ src/app.js
@@ -2,14 +2,14 @@
 
 function getNewCity(e) {
   e.preventDefault();
-  let cityInput = document.getElementById("new-city");
-  let cityInputValue = cityInput.value.trim().toLowerCase();
+  const cityInput = document.getElementById("new-city");
+  const cityInputValue = cityInput.value.trim().toLowerCase();
   getApiUrl(cityInputValue);
   cityInput.value = "";
 }
 
 function getApiUrl(city, lat, long) {
-  let apiKey = "97c2f6a3b34509ac62090edc5d18d949";
+  const apiKey = "97c2f6a3b34509ac62090edc5d18d949";
   let apiUrl;
   let unit = celsius ? "metric" : "imperial";
   if (city != "blank") {
@@ -21,8 +21,8 @@ function getApiUrl(city, lat, long) {
 }
 
 function getDayTime(utcMilliseconds) {
-  let currentDayTime = document.getElementById("current-day-time");
-  let days = [
+  const currentDayTime = document.getElementById("current-day-time");
+  const days = [
     "Sunday",
     "Monday",
     "Tuesday",
@@ -32,19 +32,18 @@ function getDayTime(utcMilliseconds) {
     "Saturday",
   ];
 
-  let date = new Date(utcMilliseconds);
-  let day = days[date.getUTCDay()];
-  let hours = String(date.getUTCHours()).padStart(2, "0");
-  let minutes = String(date.getUTCMinutes()).padStart(2, "0");
+  const date = new Date(utcMilliseconds);
+  const day = days[date.getUTCDay()];
+  const hours = String(date.getUTCHours()).padStart(2, "0");
+  const minutes = String(date.getUTCMinutes()).padStart(2, "0");
 
   return (currentDayTime.textContent = `${day} · ${hours}:${minutes}`);
 }
 
 function get5Days(utcMilliseconds) {
-  let date = new Date(utcMilliseconds);
-  console.log(date);
-  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  let day = days[date.getUTCDay()];
+  const date = new Date(utcMilliseconds);
+  const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  const day = days[date.getUTCDay()];
   return day;
 }
 
@@ -52,9 +51,7 @@ function getCityData(apiUrl, apiKey) {
   fetch(`${apiUrl}&appid=${apiKey}`).then((response) => {
     if (response.ok) {
       axios.get(`${apiUrl}&appid=${apiKey}`).then((find) => {
-        console.log(find);
         tempCelsius = find.data.main.temp;
-        // getTempType(celsius);
         let temp = Math.round(find.data.main.temp);
         let city = find.data.name;
         let humidity = find.data.main.humidity;
@@ -70,7 +67,7 @@ function getCityData(apiUrl, apiKey) {
         icon = find.data.weather[0].icon;
         changeIcon(icon, description);
         getDayTime((find.data.dt + find.data.timezone) * 1000);
-        getForecast(find.data.coord);
+        getForecast(find.data.coord, apiKey);
       });
     } else {
       alert(
@@ -80,18 +77,14 @@ function getCityData(apiUrl, apiKey) {
   });
 }
 
-function getForecast(coord) {
-  console.log(coord);
-  let apiKey = "97c2f6a3b34509ac62090edc5d18d949";
+function getForecast(coord, apiKey) {
   const fiveDayCtr = document.getElementById("five-day-ctr");
   while (fiveDayCtr.firstChild) {
     fiveDayCtr.removeChild(fiveDayCtr.firstChild);
   }
   const unit = celsius ? "metric" : "imperial";
-  let apiForecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&appid=${apiKey}&units=${unit}`;
-  console.log(apiForecastUrl);
+  const apiForecastUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coord.lat}&lon=${coord.lon}&appid=${apiKey}&units=${unit}`;
   axios.get(apiForecastUrl).then((find) => {
-    console.log(find);
     for (let i = 0; i < 5; i++) {
       const forecastCard = document.createElement("div");
       forecastCard.classList.add("col", "forecast-card");
@@ -128,10 +121,10 @@ function getForecast(coord) {
 }
 
 function formatNewCity(city, temp, humidity, wind, description) {
-  let currentTemp = document.getElementById("current-temp-value");
-  let currentHumidity = document.getElementById("current-humidity");
-  let currentWind = document.getElementById("current-wind");
-  let currentDescription = document.getElementById("current-description");
+  const currentTemp = document.getElementById("current-temp-value");
+  const currentHumidity = document.getElementById("current-humidity");
+  const currentWind = document.getElementById("current-wind");
+  const currentDescription = document.getElementById("current-description");
   currentCityCell.textContent = city;
   currentTemp.textContent = temp;
   currentDescription.textContent =
@@ -141,15 +134,15 @@ function formatNewCity(city, temp, humidity, wind, description) {
 }
 
 function changeIcon(icon, description) {
-  let currentIcon = document.getElementById("current-icon");
+  const currentIcon = document.getElementById("current-icon");
   currentIcon.src = `https://openweathermap.org/img/wn/${icon}@2x.png`;
   currentIcon.alt = description;
   dayNight(icon);
 }
 
 function dayNight(sky) {
-  let darkLight = document.querySelectorAll(".dark-light");
-  let btns = document.querySelectorAll(".btn");
+  const darkLight = document.querySelectorAll(".dark-light");
+  const btns = document.querySelectorAll(".btn");
   sky = sky.slice(2);
   if (sky === "n") {
     darkLight.forEach((element) => {
@@ -175,8 +168,8 @@ function getUserLocation() {
 }
 
 function getLatLong(position) {
-  let lat = position.coords.latitude;
-  let long = position.coords.longitude;
+  const lat = position.coords.latitude;
+  const long = position.coords.longitude;
   getApiUrl("blank", lat, long);
 }
 
@@ -193,59 +186,41 @@ function getTempType(celsius) {
     degreesC.classList.remove("hidden");
     degreesF.classList.add("hidden");
     getApiUrl(currentCityCell.textContent);
-    // currentTempCell.innerText = Math.round(tempCelsius);
   } else {
     degreesF.classList.remove("hidden");
     degreesC.classList.add("hidden");
     getApiUrl(currentCityCell.textContent);
-    // currentTempCell.innerText = Math.round((tempCelsius * 9) / 5 + 32);
   }
 }
 
-let currentCityCell = document.getElementById("current-city");
 let celsius = true;
 let icon = null;
 let tempCelsius = null;
-let currentTempCell = document.getElementById("current-temp-value");
+const currentTempCell = document.getElementById("current-temp-value");
+const currentCityCell = document.getElementById("current-city");
 
-let degreesC = document.getElementById("degrees-c");
+const degreesC = document.getElementById("degrees-c");
 degreesC.addEventListener("click", (e) => {
   e.preventDefault();
   celsius = true;
   getTempType(celsius);
 });
 
-let degreesF = document.getElementById("degrees-f");
+const degreesF = document.getElementById("degrees-f");
 degreesF.addEventListener("click", (e) => {
   e.preventDefault();
   celsius = false;
   getTempType(celsius);
 });
 
-let userSearchForm = document.getElementById("search-cities");
+const userSearchForm = document.getElementById("search-cities");
 userSearchForm.addEventListener("submit", (e) => {
   getNewCity(e);
 });
 
-let currentLocationBtn = document.getElementById("current-location-btn");
+const currentLocationBtn = document.getElementById("current-location-btn");
 currentLocationBtn.addEventListener("click", () => {
   getUserLocation();
 });
 
 getApiUrl("Bordeaux");
-
-function createForecast() {}
-
-createForecast();
-
-// function Forecast(day, icon, description, tempMin, tempMax) {
-//   return {
-//     day,
-//     icon,
-//     description,
-//     tempMin
-//     tempMax
-//   }
-// }
-
-// const getForecast = []
